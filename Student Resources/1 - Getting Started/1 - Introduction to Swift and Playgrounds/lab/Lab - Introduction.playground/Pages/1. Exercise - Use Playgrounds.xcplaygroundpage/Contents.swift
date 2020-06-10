3eb262f324f5e9c784232434c50119a577b46178
ImYazeed <imYazeedALZahrani@gmail.com>
2020-06-10 21:04:39 +0300

    ch#1 - lab #1

--- Student Resources/1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Student Resources/1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,14 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
-
+print("Song: Take Me Home, Country Roads")
+print("Artist: John Denver")
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
-
+print("Country roads, take me home")
+print("To the place I belong")
+print("West Virginia, mountain mama")
 /*:
 
  _Copyright © 2018 Apple Inc._
